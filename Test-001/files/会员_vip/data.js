e45ebebc9f87061d389cf2c1043c30cf684bc808
mike <mike@hajie.edu.kg>
2026-01-01 00:54:09 +0800

    url test

---
+++ Test-001/files/会员_vip/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,i,j,_(k,l),m,[n],o,_(p,q,r,s,t,u,v,_(),w,_(x,y,z,A,B,_(C,D,E,F),G,null,H,A,I,A,J,K,L,null,M,N,O,P,Q,R,S,N),T,_(),U,_(),V,_(W,[_(X,Y,Z,k,ba,bb,r,bc,bd,bc,be,i,w,_(x,bf,bg,_(bh,bi,bj,bk)),T,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="会员_vip.html",d="generationDate",e=new Date(1767199907957.31),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i=true,j="sketchKeys",k="",l="s0",m="variables",n="OnLoadVariable",o="page",p="packageId",q="80b012b6143540e1a11c1d67ae79d76a",r="type",s="Axure:Page",t="name",u="会员/VIP",v="notes",w="style",x="baseStyle",y="627587b6038d43cca051c114ac41ad32",z="pageAlignment",A="near",B="fill",C="fillType",D="solid",E="color",F=0xFFFFFFFF,G="image",H="imageHorizontalAlignment",I="imageVerticalAlignment",J="imageRepeat",K="auto",L="favicon",M="sketchFactor",N="0",O="colorStyle",P="appliedColor",Q="fontName",R="Applied Font",S="borderWidth",T="adaptiveStyles",U="interactionMap",V="diagram",W="objects",X="id",Y="5742f8439a3140848e9646f48614ed92",Z="label",ba="friendlyType",bb="图片",bc="imageBox",bd="styleType",be="visible",bf="ed46e34596e04eed91e6297b607bf92a",bg="size",bh="width",bi=659,bj="height",bk=414,bl="imageOverrides",bm="images",bn="normal~",bo="images/大厅_1/vip弹窗_u3321.png",bp="masters",bq="objectPaths",br="5742f8439a3140848e9646f48614ed92",bs="scriptId",bt="u3369";
+return _creator();
+})());
